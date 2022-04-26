d7a9e037cddcef3caaa5a33717ad189b54f374e0
aimarwibowo46 <aimarwibowo46@gmail.com>
2022-04-26 12:02:24 +0700

    Add Paging 3 (Unfinished) and Custom Map Style

--- app/src/main/java/com/example/dicodingstoryapp1/StoryAdapter.kt
+++ app/src/main/java/com/example/dicodingstoryapp1/StoryAdapter.kt
@@ -3,47 +3,74 @@ package com.example.dicodingstoryapp1
 import android.app.Activity
 import android.content.Intent
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.app.ActivityOptionsCompat
 import androidx.core.util.Pair
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.dicodingstoryapp1.api.ListStoryItem
+import com.example.dicodingstoryapp1.databinding.StoriesItemBinding
 import com.example.dicodingstoryapp1.model.Story
 
-class StoryAdapter(private val listStories: ArrayList<Story>) : RecyclerView.Adapter<StoryAdapter.ViewHolder>(){
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val imgPhoto: ImageView = view.findViewById(R.id.img_photo)
-        val tvName: TextView = view.findViewById(R.id.tv_name)
-    }
+class StoryAdapter : PagingDataAdapter<ListStoryItem, StoryAdapter.MyViewHolder>(DIFF_CALLBACK) {
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(viewGroup.context).inflate(R.layout.stories_item, viewGroup, false))
-    }
+    class MyViewHolder(private val binding: StoriesItemBinding) :RecyclerView.ViewHolder(binding.root){
+        fun bind(data: ListStoryItem) {
+            Glide.with(binding.root.context)
+                .load(data.photoUrl)
+                .circleCrop()
+                .into(binding.imgPhoto)
+            binding.tvName.text = data.name
 
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.tvName.text = listStories[position].name
-        Glide.with(viewHolder.itemView.context)
-            .load(listStories[position].photo)
-            .circleCrop()
-            .into(viewHolder.imgPhoto)
+            binding.root.setOnClickListener {
+                val story = Story(
+                    data.name,
+                    data.photoUrl,
+                    data.description,
+                    null,
+                    null
+                )
 
-        viewHolder.itemView.setOnClickListener {
-            val optionsCompat: ActivityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(
-                viewHolder.itemView.context as Activity,
-                Pair(viewHolder.imgPhoto, "img_photo_detail_transition"),
-                Pair(viewHolder.tvName, "tv_name_detail_transition"),
-            )
+                val optionsCompat: ActivityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                    binding.root.context as Activity,
+                    Pair(binding.imgPhoto, "img_photo_detail_transition"),
+                    Pair(binding.tvName, "tv_name_detail_transition")
+                )
 
-            val intent = Intent(viewHolder.itemView.context, DetailStoryActivity::class.java)
-            intent.putExtra(DetailStoryActivity.DETAIL_STORY, listStories[position])
-            viewHolder.itemView.context.startActivity(intent, optionsCompat.toBundle())
+                val intent = Intent(binding.root.context, DetailStoryActivity::class.java)
+                intent.putExtra(DetailStoryActivity.DETAIL_STORY, story)
+                binding.root.context.startActivity(intent, optionsCompat.toBundle())
+            }
         }
     }
 
-    override fun getItemCount(): Int {
-        return listStories.size
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = StoriesItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val data = getItem(position)
+        if(data != null) {
+            holder.bind(data)
+        }
+    }
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<ListStoryItem>() {
+            override fun areItemsTheSame(oldItem: ListStoryItem, newItem: ListStoryItem): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: ListStoryItem,
+                newItem: ListStoryItem
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+        }
     }
 }
